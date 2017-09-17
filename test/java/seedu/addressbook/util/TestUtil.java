0ea881f43abd135370581ed68bc787eb9c45eee8
Alex Ang <E0032221@u.nus.edu>
2017-09-17 18:41:30 +0800

    Organize imports

--- test/java/seedu/addressbook/util/TestUtil.java
+++ test/java/seedu/addressbook/util/TestUtil.java
@@ -25,17 +25,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-import seedu.addressbook.data.AddressBook;
-import seedu.addressbook.data.exception.IllegalValueException;
-import seedu.addressbook.data.person.Address;
-import seedu.addressbook.data.person.Email;
-import seedu.addressbook.data.person.Name;
-import seedu.addressbook.data.person.Person;
-import seedu.addressbook.data.person.Phone;
-import seedu.addressbook.data.person.ReadOnlyPerson;
-import seedu.addressbook.data.person.UniquePersonList;
-import seedu.addressbook.data.tag.Tag;
-import seedu.addressbook.data.tag.UniqueTagList;
 import seedu.addressbook.state.ApplicationHistory;
 
 public class TestUtil {
